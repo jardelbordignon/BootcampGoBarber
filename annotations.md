85a6a4f6f6494fa6bc79cda4be1cb2e67445a595
Jardel <jardel1101@gmail.com>
2021-01-25 10:44:15 -0300

    Init backend

---
+++ annotations.md
@@ -0,0 +1,6 @@
+> Init backend
+1. Add dependencies express | -D typescript ts-node-dev @types/express
+3. Create tsconfig.json *yarn tsc --init*
+4. Configure outDir and rootDir in tsconfig.json
+5. Create src/server.ts
+6. Inlcude scripts in package.json
---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+})
+
+const port = 3333
+app.listen(port, () => console.log('✨ Server running on port '+port))
