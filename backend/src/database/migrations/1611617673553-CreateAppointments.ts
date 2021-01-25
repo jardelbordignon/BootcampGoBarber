8ff4c027514dd67c0b94545643c5f454a969f2a7
Jardel <jardel1101@gmail.com>
2021-01-25 20:50:32 -0300

    CreateAppointments migration

---
+++ backend/src/database/migrations/1611617673553-CreateAppointments.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm'
+
+export default class CreateAppointments1611617673553 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            //default: 'generate()'
+          },
+          {
+            name: 'provider',
+            type: 'varchar'
+          },
+          {
+            name: 'date',
+            type: 'timestamp'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments')
+  }
+
+}
