924f7aef573f5f04352a6c5ee0a2f35c87198396
Jardel <jardel1101@gmail.com>
2021-01-25 22:43:32 -0300

    Model and migration users

---
+++ backend/src/models/User.ts
@@ -0,0 +1,22 @@
+import { Column, Entity } from 'typeorm'
+import Defaults from './Defaults'
+
+export interface CreateUserDTO {
+  name: string
+  email: string
+  password: string
+}
+
+@Entity('users')
+export default class User extends Defaults {
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+}
