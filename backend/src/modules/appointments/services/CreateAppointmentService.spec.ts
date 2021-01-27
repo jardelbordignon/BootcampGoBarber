f972c8460de186c6fdbdad9665bd40b99177e3ca
Jardel <jardel1101@gmail.com>
2021-01-27 19:00:32 -0300

    Appointment tests

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@/shared/errors/AppError'
 import FakeAppointmentsRepository from '@/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts'
 import CreateAppointmentService from './CreateAppointmentService'
 
@@ -16,8 +17,23 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123abc')
   })
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1+2).toBe(3)
-  // })
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository)
+
+    const appointmentDate = new Date(2021, 1, 27, 11)
+
+    await createAppointmentService.execute({
+      provider_id: '123abc',
+      date: appointmentDate
+    })
+
+    expect(
+      createAppointmentService.execute({
+        provider_id: '123abc',
+        date: appointmentDate
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
 
 })
