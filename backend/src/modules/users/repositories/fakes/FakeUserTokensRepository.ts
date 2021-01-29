e17cd8038f7cd3a3ef16f790f134a749967112e3
Jardel <jardel1101@gmail.com>
2021-01-29 17:00:21 -0300

    Completing password recovery

---
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,23 @@
+import { generate } from 'shortid'
+
+import IUserTokensRepository from '@/modules/users/repositories/IUserTokensRepository'
+import UserToken from '@/modules/users/infra/typeorm/entities/UserToken'
+
+export default class UserTokensRepository implements IUserTokensRepository {
+
+  private userTokens: UserToken[] = []
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken()
+
+    userToken.id = generate()
+    userToken.token = generate()
+    userToken.user_id = user_id
+
+    this.userTokens.push(userToken)
+
+    return userToken
+  }
+
+}
+
