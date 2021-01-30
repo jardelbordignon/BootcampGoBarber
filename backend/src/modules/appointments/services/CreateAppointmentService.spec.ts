7214b00bc677a33cee4cbeff8da261c57107a486
Jardel <jardel1101@gmail.com>
2021-01-30 20:01:17 -0300

    Refactored tests

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,12 +2,17 @@ import AppError from '@/shared/errors/AppError'
 import FakeAppointmentsRepository from '@/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts'
 import CreateAppointmentService from './CreateAppointmentService'
 
-describe('CreateAppointment', () => {
+let fakeAppointmentsRepository: FakeAppointmentsRepository
+let createAppointmentService: CreateAppointmentService
+
+describe('CreateAppointmentService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository)
+
+  })
 
   it('should be able to create a new appointment', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
-    const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository)
-
     const appointment = await createAppointmentService.execute({
       provider_id: '123abc',
       date: new Date()
@@ -18,9 +23,6 @@ describe('CreateAppointment', () => {
   })
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
-    const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository)
-
     const appointmentDate = new Date(2021, 1, 27, 11)
 
     await createAppointmentService.execute({
