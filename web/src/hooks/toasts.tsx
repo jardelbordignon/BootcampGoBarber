32afd795af1bf38134bb20aef5660f0da1961321
Jardel <jardel1101@gmail.com>
2021-02-13 05:50:04 -0300

    Add and remove Toasts

--- web/src/hooks/toasts.tsx
+++ web/src/hooks/toasts.tsx
@@ -1,28 +1,39 @@
-import { createContext, useContext, useCallback } from 'react'
+import { createContext, useContext, useCallback, useState } from 'react'
+import { generate } from 'shortid'
 
 import Toasts from '../components/Toasts'
 
+export interface IToast {
+  id: string
+  title: string
+  description?: string
+  type?: 'info' | 'success' | 'error'
+}
+
 interface IToastsContext {
-  addToast(): void
-  removeToast(): void
+  addToast(toast: Omit<IToast, 'id'>): void
+  removeToast(id: string): void
 }
 
 const ToastsContext = createContext<IToastsContext>({} as IToastsContext)
 
 export const ToastsProvider: React.FC = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('addToast')
+  const [toasts, setToasts] = useState<IToast[]>([])
+
+  const addToast = useCallback(({ title, description, type }: Omit<IToast, 'id'>) => {
+    const toast = { id: generate(), title, description, type }
+    setToasts((state) => [...state, toast])
   }, [])
 
-  const removeToast = useCallback(() => {
-    console.log('removeToast')
+  const removeToast = useCallback((id: string) => {
+    setToasts((state) => state.filter((toast) => toast.id !== id))
   }, [])
 
   return (
     <ToastsContext.Provider value={{ addToast, removeToast }}>
       {children}
 
-      <Toasts />
+      <Toasts toasts={toasts} />
     </ToastsContext.Provider>
   )
 }
