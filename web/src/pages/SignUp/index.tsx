559b8fd8bc04c8c5b8eb1a030c1ebdb6540cb6b0
Jardel <jardel1101@gmail.com>
2021-02-14 00:33:37 -0300

    Conditioned Routes, SignIn and SignOut ok

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -3,8 +3,10 @@ import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import api from '../../services/api'
 
+import { useToasts } from '../../hooks/toasts'
 import getValidationsErrors from '../../utils/getValidationsErrors'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
@@ -13,7 +15,7 @@ import logo from '../../assets/logo.svg'
 
 import { AnimatedContainer, Container, Content, Background } from './styles'
 
-interface Props {
+interface ISignUp {
   name: string
   email: string
   password: string
@@ -21,24 +23,46 @@ interface Props {
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
+  const { addToast } = useToasts()
+  const history = useHistory()
 
-  const handleSubmit = useCallback(async (data: Props) => {
-    try {
-      formRef.current?.setErrors({})
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome é obrigatório'),
-        email: Yup.string().required('E-mail é obrigatório').email('E-mail inválido'),
-        password: Yup.string().min(6, 'Senha no mínimo 6 caracteres'),
-      })
+  const handleSubmit = useCallback(
+    async (data: ISignUp) => {
+      try {
+        formRef.current?.setErrors({})
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório'),
+          email: Yup.string().required('E-mail é obrigatório').email('E-mail inválido'),
+          password: Yup.string().min(6, 'Senha no mínimo 6 caracteres'),
+        })
 
-      await schema.validate(data, { abortEarly: false })
-    } catch (error) {
-      console.log(error)
+        await schema.validate(data, { abortEarly: false })
 
-      const errors = getValidationsErrors(error)
-      formRef.current?.setErrors(errors)
-    }
-  }, [])
+        await api.post('/users', data)
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Faça seu login no GoBarber',
+        })
+
+        history.push('/')
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationsErrors(error)
+          formRef.current?.setErrors(errors)
+          return
+        }
+        // trigger a toast
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer seu cadastro. Tente novamente',
+        })
+      }
+    },
+    [addToast, history]
+  )
 
   return (
     <Container>
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import ConditionedRoute from '../routes/ConditionedRoute'
+
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
+import Dashboard from '../pages/Dashboard'
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/signup" exact component={SignUp} />
+      <ConditionedRoute path="/" exact component={SignIn} />
+      <ConditionedRoute path="/signup" component={SignUp} />
+
+      <ConditionedRoute path="/dash" component={Dashboard} isPrivate />
     </Switch>
   </BrowserRouter>
 )
