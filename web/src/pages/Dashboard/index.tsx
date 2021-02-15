c0214b707b5586666704d871734d475fbc12ee19
Jardel <jardel1101@gmail.com>
2021-02-15 16:37:47 -0300

    Next appointment

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
-import { Container, Header, HeaderContent, Profile } from './styles'
+import { FiPower, FiUser, FiClock } from 'react-icons/fi'
 
-import logo from '../../assets/logo.svg'
-import { FiPower, FiUser } from 'react-icons/fi'
 import { useAuth } from '../../hooks/auth'
+import logo from '../../assets/logo.svg'
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles'
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth()
@@ -23,6 +23,30 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Próximo agendamento</strong>
+            <div>
+              <img src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
+              <strong>John Doe</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar>Calendar</Calendar>
+      </Content>
     </Container>
   )
 }
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -56,3 +56,87 @@ export const Profile = styled.div`
     > strong { color: ${theme.colors.primary} }
   }
 `
+
+export const Content = styled.main`
+  display: flex;
+  max-width: 1120px;
+  margin: 64px auto;
+`
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 { font-size: 36px; }
+
+  p {
+    margin-top: 8px;
+    color: ${theme.colors.primary};
+
+    span {
+      padding-right: 10px;
+    }
+    > span + span {
+      padding-left: 10px;
+      border-left: 1px solid ${theme.colors.primary};
+    }
+  }
+`
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  strong {
+    color: ${shade(0.2, theme.colors.white)};
+    font-size: 20px;
+    font-weight: 400px;
+  }
+
+  > div {
+    background-color: ${theme.colors.secondary};
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    // margem lateral
+    &::before {
+      content: '';
+      position: absolute;
+      height: 80%;
+      width: 2px;
+      left: 0;
+      top: 10px;
+      background-color: ${theme.colors.primary};
+    }
+
+    > img {
+      height: 80px;
+      min-width: 80px;
+      border-radius: 50%;
+    }
+
+    > strong {
+      font-size: 24px;
+      margin-left: 24px;
+    }
+
+    > span {
+      display: flex;
+      align-items: center;
+      color: ${shade(0.2, theme.colors.white)};
+      margin-left: auto;
+      > svg {
+        font-size: 24px;
+        margin: 0 10px 0 24px;
+        color: ${theme.colors.primary};
+      }
+    }
+  }
+`
+
+export const Calendar = styled.aside`
+  width: 380px;
+`
