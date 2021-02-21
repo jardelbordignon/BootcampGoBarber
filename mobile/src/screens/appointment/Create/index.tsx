6e1c69bce19751e443992d0b4ffb35e9d574a71f
Jardel <jardel1101@gmail.com>
2021-02-21 13:01:04 -0300

    Appointments ok

--- mobile/src/screens/appointment/Create/index.tsx
+++ mobile/src/screens/appointment/Create/index.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback, useMemo } from 'react'
+import { Platform, Alert } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+import DateTimePicker from '@react-native-community/datetimepicker'
+import { format } from 'date-fns'
 
 import api from '../../../services/api'
 import { useAuth } from '../../../hooks/auth'
@@ -12,10 +15,23 @@ import {
   BackButton,
   HeaderTitle,
   Avatar,
+  Content,
   ProvidersListContainer,
   ProvidersList,
   ProviderContainer,
   ProviderName,
+  Calendar,
+  Title,
+  OpenDatePickerButton,
+  OpenDatePickerButtonText,
+  Schedule,
+  Section,
+  SectionTitle,
+  SectionContent,
+  Hour,
+  HourText,
+  CreateAppointmentButton,
+  CreateAppointmentButtonText
 } from './styles'
 
 interface RouteParams {
@@ -28,13 +44,23 @@ export interface IProvider {
   avatar: string
 }
 
+interface IAvailabilityItem {
+  hour: number
+  available: boolean
+}
+
 const CreateAppointment: React.FC = () => {
   const route = useRoute()
   const routeParams = route.params as RouteParams
   const { user } = useAuth()
-  const { goBack } = useNavigation()
+  const { goBack, navigate } = useNavigation()
   const [providers, setProviders] = useState<IProvider[]>([])
   const [selectedProvider, setSelectedProvider] = useState(routeParams.providerId)
+  const [showDatePicker, setShowDatePicker] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const [availability, setAvailability] = useState<IAvailabilityItem[]>([])
+  const [selectedHour, setSelectedHour] = useState(0)
+
 
   useEffect(() => {
     api.get('/providers').then((response) => {
@@ -42,9 +68,71 @@ const CreateAppointment: React.FC = () => {
     })
   }, [])
 
+  useEffect(() => {
+    api.get(`/providers/${selectedProvider}/day-availability`, {
+      params: {
+        year: selectedDate.getFullYear(),
+        month: selectedDate.getMonth() + 1,
+        day: selectedDate.getDate()
+      }
+    }).then((response) => setAvailability(response.data))
+  }, [selectedDate, selectedProvider])
+
   const selectProviderHandler = useCallback((providerId: string) => {
     setSelectedProvider(providerId)
   }, [])
+
+  const toggleDatePickerHandler = useCallback(() => {
+    setShowDatePicker(state => !state)
+  }, [])
+
+  const onChangeDatePickerHandler = useCallback((event: Event, date?: Date | undefined) => {
+    if (Platform.OS === 'android') setShowDatePicker(false)
+    if (date) setSelectedDate(date)
+  }, [])
+
+  const selectedHourHandler = useCallback((hourNumber) => {
+    setSelectedHour(hourNumber)
+  }, [])
+
+  const createAppointmentHandler = useCallback(async() => {
+    try {
+      const date = new Date(selectedDate)
+      date.setHours(selectedHour)
+      date.setMinutes(0)
+
+      await api.post('/appointments', { provider_id: selectedProvider, date })
+
+      navigate('ShowAppointment', { date: date.getTime() })
+    } catch (error) {
+      console.log(error)
+      Alert.alert(
+        'Erro ao criar o agendamento',
+        'Ocorreu um erro ao criar o agendamento, tente novamente'
+      )
+    }
+  }, [selectedDate, selectedHour, selectedProvider, navigate])
+
+  const morningAvailability = useMemo(() => (
+    availability
+      .filter(({hour}) => hour < 12)
+      .map(({hour, available}) => ({
+        hour,
+        available,
+        formattedHour: format(new Date().setHours(hour), 'HH:00')
+      }))
+  ), [availability])
+
+  const afternoonAvailability = useMemo(() => (
+    availability
+      .filter(({hour}) => hour >= 12)
+      .map(({hour, available}) => ({
+        hour,
+        available,
+        formattedHour: format(new Date().setHours(hour), 'HH:00')
+      }))
+  ), [availability])
+
 
   return (
     <Container>
@@ -58,23 +146,85 @@ const CreateAppointment: React.FC = () => {
         <Avatar source={{ uri: user.avatar_url }} />
       </Header>
 
-      <ProvidersListContainer>
-        <ProvidersList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={providers}
-          keyExtractor={(provider) => provider.id}
-          renderItem={({ item: provider }) => (
-            <ProviderContainer
-              selected={selectedProvider === provider.id}
-              onPress={() => selectProviderHandler(provider.id)}
-            >
-              {provider.avatar_url && <Avatar size={32} source={{ uri: provider.avatar_url }} />}
-              <ProviderName selected={selectedProvider === provider.id}>{provider.name}</ProviderName>
-            </ProviderContainer>
-          )}
-        />
-      </ProvidersListContainer>
+      <Content>
+        <ProvidersListContainer>
+          <ProvidersList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={providers}
+            keyExtractor={(provider) => provider.id}
+            renderItem={({ item: provider, index }) => {
+              const selected = selectedProvider === provider.id
+              return(
+                <ProviderContainer key={index} selected={selectedProvider === provider.id} onPress={() => selectProviderHandler(provider.id)}>
+                  {provider.avatar_url && <Avatar size={32} source={{ uri: provider.avatar_url }} />}
+                  <ProviderName selected={selected}>{provider.name}</ProviderName>
+                </ProviderContainer>
+              )
+            }}
+          />
+        </ProvidersListContainer>
+
+        <Calendar>
+          <Title>Escolha a data</Title>
+          <OpenDatePickerButton onPress={toggleDatePickerHandler}>
+            <OpenDatePickerButtonText>Selecionar outra data</OpenDatePickerButtonText>
+          </OpenDatePickerButton>
+          { showDatePicker &&
+            <DateTimePicker
+              {...(Platform.OS === 'ios' && { textColor: '#f4ede8' })} // < nessa linha
+              mode="date"
+              display={Platform.OS === 'android' ? 'calendar' : 'spinner'}
+              onChange={onChangeDatePickerHandler}
+              value={selectedDate}
+
+            />
+          }
+        </Calendar>
+
+        <Schedule>
+          <Title>Escolha o horário</Title>
+
+          <Section>
+            <SectionTitle>Manhã</SectionTitle>
+            <SectionContent>
+              { morningAvailability.map(({ formattedHour, hour, available }) => (
+                <Hour
+                  key={formattedHour}
+                  available={available}
+                  enabled={available}
+                  onPress={() => selectedHourHandler(hour)}
+                  selected={selectedHour === hour}>
+                  <HourText>{formattedHour}</HourText>
+                </Hour>
+              )) }
+            </SectionContent>
+          </Section>
+
+          <Section>
+            <SectionTitle>Tarde</SectionTitle>
+            <SectionContent>
+              { afternoonAvailability.map(({ formattedHour, hour, available }) => (
+                <Hour
+                  key={formattedHour}
+                  available={available}
+                  enabled={available}
+                  onPress={() => selectedHourHandler(hour)}
+                  selected={selectedHour === hour}>
+                  <HourText selected={selectedHour === hour}>{formattedHour}</HourText>
+                </Hour>
+              )) }
+            </SectionContent>
+          </Section>
+        </Schedule>
+
+        <CreateAppointmentButton onPress={createAppointmentHandler}>
+          <CreateAppointmentButtonText>
+            Agendar
+          </CreateAppointmentButtonText>
+        </CreateAppointmentButton>
+      </Content>
+
     </Container>
   )
 }
--- mobile/src/screens/appointment/Create/styles.ts
+++ mobile/src/screens/appointment/Create/styles.ts
@@ -40,6 +40,8 @@ export const Avatar = styled.Image<IAvatar>`
   border-radius: ${props => props.size ? props.size/2 : 28}px;
 `
 
+export const Content = styled.ScrollView``
+
 export const ProvidersListContainer = styled.View`
   height: 112px;
 `
@@ -67,5 +69,87 @@ export const ProviderName = styled.Text<IProviderName>`
   margin-left: 8px;
   font-family: 'RobotoSlab-Medium';
   font-size: 16px;
-  color: ${props => theme.colors[props.selected ? 'secondary' : 'white']};;
+  color: ${props => theme.colors[props.selected ? 'secondary' : 'white']};
+`
+
+export const Calendar = styled.View``
+
+export const Title = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 24px;
+  margin: 0 24px 24px;
+  color: ${theme.colors.white};
+`
+
+export const OpenDatePickerButton = styled(RectButton)`
+  background: ${theme.colors.primary};
+  height: 46px;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 24px;
+`
+
+export const OpenDatePickerButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${theme.colors.background};
+`
+
+export const Schedule = styled.View`
+  padding: 24px 0 16px;
+`
+
+export const Section = styled.View`
+  margin-bottom: 24px;
+`
+
+export const SectionTitle = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: ${theme.colors.white};
+  margin: 20px 24px;
+`
+
+export const SectionContent = styled.ScrollView.attrs({
+  contentContainerStyle: { paddingHorizontal: 24 },
+  horizontal: true,
+  showsHorizontalScrollIndicator: false
+})``
+
+interface IHour {
+  available: boolean
+  selected: boolean
+}
+export const Hour = styled(RectButton)<IHour>`
+  padding: 12px;
+  background: ${props => props.selected ? theme.colors.primary : theme.colors.secondary};
+  border-radius: 10px;
+  margin-right: 8px;
+  opacity: ${props => props.available ? 1 : 0.3};
+`
+
+interface IHourText {
+  selected: boolean
+}
+export const HourText = styled.Text<IHourText>`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+  color: ${props => props.selected ? theme.colors.secondary : theme.colors.white};
+`
+
+
+export const CreateAppointmentButton = styled(RectButton)`
+  background: ${theme.colors.primary};
+  height: 50px;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 24px 30px;
+`
+
+export const CreateAppointmentButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: ${theme.colors.secondary};
 `
